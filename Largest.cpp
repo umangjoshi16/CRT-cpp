441d2f53734aa0baec393372b6a56e63a5adbfa3
umangjoshi16 <umangjoshi744@gmail.com>
2026-05-05 10:52:06 +0530

    Largest

---
+++ Largest.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"Enter the numbers :"<<endl;
+    cin>>a>>b>>c;
+
+    if(a>b && a>c){
+        
+            cout<<"Largest number is :"<<a<<endl;
+    }
+    else if(b>a && b>c){
+        cout<<"Largest number is :"<<b<<endl;
+
+    }
+
+    else{
+        
+            cout<<"Largest number is :"<<c<<endl;
+    }
+
+        
+    
+    return 0;
+
+}
