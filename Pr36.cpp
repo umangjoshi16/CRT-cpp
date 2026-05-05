072960461525c8505237146826e40d61d02ba145
umangjoshi16 <umangjoshi744@gmail.com>
2026-05-05 11:43:38 +0530

    Swap 2 numbers

---
+++ Pr36.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void swapNumbers(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x, y;
+
+    cout << "Enter two numbers: ";
+    cin >> x >> y;
+
+    swapNumbers(&x, &y);  
+
+    cout << "After swapping:\n";
+    cout << "x = " << x << ", y = " << y << endl;
+
+    return 0;
+}
